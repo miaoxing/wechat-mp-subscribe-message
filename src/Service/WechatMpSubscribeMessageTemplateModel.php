58dc347938c451a987618969add11154a140eac7
twinh <twinhuang@qq.com>
2025-01-17 16:43:33 +0800

    refactor(internal): 生成元数据到模型类中，删除 Trait

--- src/Service/WechatMpSubscribeMessageTemplateModel.php
+++ src/Service/WechatMpSubscribeMessageTemplateModel.php
@@ -6,14 +6,26 @@ use Miaoxing\Plugin\BaseModel;
 use Miaoxing\Plugin\Model\ModelTrait;
 use Miaoxing\Plugin\Model\ReqQueryTrait;
 use Miaoxing\Plugin\Model\SnowflakeTrait;
-use Miaoxing\WechatMpSubscribeMessage\Metadata\WechatMpSubscribeMessageTemplateTrait;
 use Wei\Model\SoftDeleteTrait;
 
+/**
+ * @property string|null $id
+ * @property string $name 名称
+ * @property string $code 标识
+ * @property string $templateId 模板编号
+ * @property string $page 跳转页面
+ * @property array $data 模板内容
+ * @property string|null $createdAt
+ * @property string|null $updatedAt
+ * @property string $createdBy
+ * @property string $updatedBy
+ * @property string|null $deletedAt
+ * @property string $deletedBy
+ */
 class WechatMpSubscribeMessageTemplateModel extends BaseModel
 {
     use ModelTrait;
     use ReqQueryTrait;
     use SnowflakeTrait;
     use SoftDeleteTrait;
-    use WechatMpSubscribeMessageTemplateTrait;
 }
